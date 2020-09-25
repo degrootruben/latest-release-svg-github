4260a0bae4d566d3590f39c1e3a984a62a124f91
degrootruben <rpg.degroot@gmail.com>
2020-09-25 21:44:23 +0200

    Added some functions and deleted some files

---
+++ index.js
@@ -0,0 +1,11 @@
+let username;
+let repository;
+
+exports.set = function(username, repository) {
+    this.username = username;
+    this.repository = repository;
+}
+
+exports.generateSVG = async function() {
+    
+}
