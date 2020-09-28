d0fbdc3dab9b84277781e41f7e03124b1340708b
Ruben de Groot <rpg.degroot@gmail.com>
2020-09-28 20:33:40 +0200

    Now using badge-maker module to generate SVG

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-let username;
-let repository;
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+const { makeBadge } = require('badge-maker');
+const method = LRSG.prototype;
+const api = "https://api.github.com";
 
-exports.set = function(username, repository) {
-    this.username = username;
-    this.repository = repository;
+function LRSG() {
+
 }
 
-exports.generateSVG = async function() {
-    
+method.generateSVG = async function(owner, repo, filePath, fileName, prefix = "newest release", color = "#4c1") {
+    return new Promise(async function(resolve, reject) {
+        try {
+            const url = `${api}/repos/${owner}/${repo}/releases`;
+            console.log(url);
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/vnd.github.v3+json"
+                }
+            });
+            const latestRelease = await response.json();
+
+            const newestReleaseDate = new Date(Math.max(...latestRelease.map(element => new Date(element.published_at))));
+            const newestRelease = latestRelease.find(e => new Date(e.published_at).getTime() === newestReleaseDate.getTime());
+            const tagName = newestRelease.tag_name;
+            
+            const format = {
+                label: prefix,
+                message: tagName,
+                color: color,
+            };
+               
+            const svg = makeBadge(format);
+            
+            fs.writeFileSync(path.join(filePath, fileName), svg);
+            resolve("Resolved");
+        } catch(err) {
+            reject(err);
+        }
+    });
 }
+
+module.exports = LRSG;
